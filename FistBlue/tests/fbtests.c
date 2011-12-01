353f32a4c0d45ae5aa805f21ed3647d54620d48e
ben <ben@30d454cc-f4c7-4f47-a1fd-a2c4f724ef71>
2011-12-01 22:34:19 +0000

    added fbtests.*
    git-svn-id: svn+ssh://svn.luckybulldozer.com/srv/svn/luckybulldozer/mt2/MT2@90 30d454cc-f4c7-4f47-a1fd-a2c4f724ef71

---
+++ FistBlue/tests/fbtests.c
@@ -0,0 +1,63 @@
+/*
+ *  fbtests.c
+ *  MT2
+ *
+ *  Created by Ben on 30/11/11.
+ *  Copyright 2011 Ben Torkington. All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <assert.h>
+
+#include "sf2types.h"
+#include "sf2const.h"
+#include "sf2macros.h"
+
+#include "gstate.h"
+#include "structs.h"
+
+#include "fbtests.h"
+#include	"lib.h"
+
+
+extern Game g;
+
+void fistblue_run_tests(void) {
+	int i;
+	memclear(&g, sizeof(struct game));	
+	
+	printf("fistblue_run_tests:\n");
+	
+#ifdef SF2_ENDIAN_LITTLE
+	printf("SF2_ENDIAN_LITTLE\n");
+#endif
+#ifdef SF2_ENDIAN_BIG
+	printf("SF2_ENDIAN_BIG\n");
+#endif
+	
+	// test sf2rand();
+	printf("output of sf2rand: ");
+	g.randSeed1    = 0x01;       /* initial random seed */
+	g.randSeed2    = 0xc2;
+	assert(sf2rand() == 0xc7);
+	assert(sf2rand() == 0xd8);
+	assert(sf2rand() == 0x0d);
+	assert(sf2rand() == 0xac);
+	printf("PASSED \n");
+	
+	printf("fixed-precision types in types.h: ");
+	FIXED8_8 testorder16;
+	FIXED16_16 testorder32;
+	
+	testorder16.full= 0x1234;
+	testorder32.full= 0x12345678;
+	assert(testorder16.part.integer == 0x12);
+	assert(testorder16.part.fraction == 0x34);
+	assert(testorder32.part.integer == 0x1234);
+	assert(testorder32.part.fraction == 0x5678);
+	printf("PASSED \n");
+	
+	
+	
+	exit(0);
+}
---
+++ FistBlue/tests/fbtests.h
@@ -0,0 +1,10 @@
+/*
+ *  fbtests.h
+ *  MT2
+ *
+ *  Created by Ben on 30/11/11.
+ *  Copyright 2011 Ben Torkington. All rights reserved.
+ *
+ */
+
+void fistblue_run_tests(void);
