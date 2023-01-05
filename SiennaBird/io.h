c15f827b67b261fefdeecfb5656220d2762d3c1e
Ben Torkington <prmera@gmail.com>
2023-01-05 20:50:32 +1300

    use the more standardised types from stdint.h

--- SiennaBird/io.h
+++ SiennaBird/io.h
@@ -7,28 +7,28 @@
  *
  */
 
-#include <unistd.h>
+#include <stdint.h>
 
 struct cps_a_regs {
     // base address registers /256
     // e.g. object_base might be 0x9020, meaning sprites begin at 0x902000
     
-    u_int16_t object_base;
-    u_int16_t scroll1_base;
-    u_int16_t scroll2_base;
-    u_int16_t scroll3_base;
-    u_int16_t rowscroll_base;
-    u_int16_t palette_base;
+    uint16_t object_base;
+    uint16_t scroll1_base;
+    uint16_t scroll2_base;
+    uint16_t scroll3_base;
+    uint16_t rowscroll_base;
+    uint16_t palette_base;
 
     // scroll displacements
     
-    u_int16_t scroll1x, scroll1y;   // offset 0xc
-    u_int16_t scroll2x, scroll2y;
-    u_int16_t scroll3x, scroll3y;
-    u_int16_t star1x, star1y;
-    u_int16_t star2x, star2y;
+    uint16_t scroll1x, scroll1y;   // offset 0xc
+    uint16_t scroll2x, scroll2y;
+    uint16_t scroll3x, scroll3y;
+    uint16_t star1x, star1y;
+    uint16_t star2x, star2y;
 
-    u_int16_t rowscroll_offset;
-    u_int16_t video_control;
+    uint16_t rowscroll_offset;
+    uint16_t video_control;
 };
 
